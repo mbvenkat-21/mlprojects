b729c4215e5b118350326e24a49335cb912e8307
Venkat <mbvenki@gmail.com>
2024-05-09 19:18:31 +0530

    First time changes to exception py file.
    First time changes to exception py file.

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 from src.logger import logging
-
+# First time push desktop Git
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
